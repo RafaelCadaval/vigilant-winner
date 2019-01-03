dbe9a94ba879e05ebdef7af7c2f39fcb744f6545
Rafael Cadaval <rafaelcadaval@gmail.com>
2019-01-03 21:23:28 -0200

    Initial commit
    Adds initial xcode project and .gitignore file.

---
+++ tech-challenge-1/tech-challenge-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tech-challenge-1
+//
+//  Created by Rafael Cadaval on 03/01/19.
+//  Copyright © 2019 Rafael Cadaval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
